07f1db0385bd1345880be44b084c451fc50232e3
shohame <33593461+shohame@users.noreply.github.com>
2023-03-06 17:38:11 +0200

    Update bandit31.py

--- Bandit/code/bandit31.py
+++ Bandit/code/bandit31.py
@@ -4,6 +4,14 @@
 
 """
 
+Bandit Level 31 → Level 32
+Level Goal
+There is a git repository at ssh://bandit31-git@localhost/home/bandit31-git/repo. The password for the user bandit31-git is the same as for the user bandit31.
+
+Clone the repository and find the password for the next level.
+
+Commands you may need to solve this level
+
 
 ssh bandit31@bandit.labs.overthewire.org -p 2220
 
