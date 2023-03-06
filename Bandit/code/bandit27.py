1f7316606d68a19c8253547b431cc9854174dfac
shohame <33593461+shohame@users.noreply.github.com>
2023-03-06 17:36:12 +0200

    Update bandit27.py

--- Bandit/code/bandit27.py
+++ Bandit/code/bandit27.py
@@ -4,6 +4,14 @@
 
 """
 
+Bandit Level 27 → Level 28
+Level Goal
+There is a git repository at ssh://bandit27-git@localhost/home/bandit27-git/repo. The password for the user bandit27-git is the same as for the user bandit27.
+
+Clone the repository and find the password for the next level.
+
+Commands you may need to solve this level
+
 
 ssh bandit27@bandit.labs.overthewire.org -p 2220
 
