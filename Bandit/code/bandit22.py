8d8bccad92f0aa916370dedd30c6e8e8a577e669
shohame <33593461+shohame@users.noreply.github.com>
2023-03-06 17:33:33 +0200

    Update bandit22.py

--- Bandit/code/bandit22.py
+++ Bandit/code/bandit22.py
@@ -4,6 +4,15 @@
 
 """
 
+Bandit Level 22 → Level 23
+Level Goal
+A program is running automatically at regular intervals from cron, the time-based job scheduler. Look in /etc/cron.d/ for the configuration and see what command is being executed.
+
+NOTE: Looking at shell scripts written by other people is a very useful skill. The script for this level is intentionally made easy to read. If you are having problems understanding what it does, try executing it to see the debug information it prints.
+
+Commands you may need to solve this level
+cron, crontab, crontab(5) (use “man 5 crontab” to access this)
+
 
 ssh bandit22@bandit.labs.overthewire.org -p 2220
 
