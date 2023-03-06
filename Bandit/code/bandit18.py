5d29fefa5d4723df8427a797092f748d46edd576
shohame <33593461+shohame@users.noreply.github.com>
2023-03-06 17:31:35 +0200

    Update bandit18.py

--- Bandit/code/bandit18.py
+++ Bandit/code/bandit18.py
@@ -4,6 +4,13 @@
 
 """
 
+Bandit Level 18 → Level 19
+Level Goal
+The password for the next level is stored in a file readme in the homedirectory. Unfortunately, someone has modified .bashrc to log you out when you log in with SSH.
+
+Commands you may need to solve this level
+ssh, ls, cat
+
 
 ssh bandit18@bandit.labs.overthewire.org -p 2220
 
