2add27d37a7fe024fef92ba13e217a83fe8952ad
shohame <33593461+shohame@users.noreply.github.com>
2023-03-06 17:32:39 +0200

    Update bandit20.py

--- Bandit/code/bandit20.py
+++ Bandit/code/bandit20.py
@@ -4,6 +4,15 @@
 
 """
 
+Bandit Level 20 → Level 21
+Level Goal
+There is a setuid binary in the homedirectory that does the following: it makes a connection to localhost on the port you specify as a commandline argument. It then reads a line of text from the connection and compares it to the password in the previous level (bandit20). If the password is correct, it will transmit the password for the next level (bandit21).
+
+NOTE: Try connecting to your own network daemon to see if it works as you think
+
+Commands you may need to solve this level
+ssh, nc, cat, bash, screen, tmux, Unix ‘job control’ (bg, fg, jobs, &, CTRL-Z, …)
+
 
 ssh bandit20@bandit.labs.overthewire.org -p 2220
 
