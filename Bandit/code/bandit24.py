83c82bf21845515c9bf0dd9e44187abd5945fbf6
shohame <33593461+shohame@users.noreply.github.com>
2023-03-06 17:34:28 +0200

    Update bandit24.py

--- Bandit/code/bandit24.py
+++ Bandit/code/bandit24.py
@@ -4,6 +4,11 @@
 
 """
 
+Bandit Level 24 → Level 25
+Level Goal
+A daemon is listening on port 30002 and will give you the password for bandit25 if given the password for bandit24 and a secret numeric 4-digit pincode. There is no way to retrieve the pincode except by going through all of the 10000 combinations, called brute-forcing.
+You do not need to create new connections each time
+
 
 ssh bandit24@bandit.labs.overthewire.org -p 2220
 
